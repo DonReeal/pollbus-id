a86f8caac235e89a93f93c16298a0c6471182aa1
donreeal <donreeal@pollbus.eu>
2016-06-20 18:03:50 +0200

    using jitpack to build baratine master snapshot as dependency

--- src/test/java/pollbus/idgen/barflake/PollIdDefaultServiceTest.java
+++ src/test/java/pollbus/idgen/barflake/PollIdDefaultServiceTest.java
@@ -38,10 +38,10 @@ public class PollIdDefaultServiceTest {
 	@Test
 	public void syncEndpointAvailable() {
 	  
-	    long value = idGen.next();		
-	    assertThat(value, notNullValue());
-	    
-        System.out.println("received a barflake: " + stringifyId(value));        
+		long value = idGen.next();		
+		assertThat(value, notNullValue());
+		
+		System.out.println("received a barflake: " + stringifyId(value));        
 	}	
 	
 
@@ -50,64 +50,64 @@ public class PollIdDefaultServiceTest {
 
 	@Test 
 	public void asyncYieldsFifeThousandIdsPerSecond() throws InterruptedException {
-	  
-        int secondsToRun = 15;
-        int ypsMinExpected = 100_000;    
-        int maxAPICalls = 15_000_000;
-        
-	    CountDownLatch resultsCountDown = new CountDownLatch(maxAPICalls);
-	    ExecutorService taskRunner = Executors.newSingleThreadExecutor();
-	    Runnable queryAPI = new Runnable() {
-          @Override
-          public void run() {
-            for(int i = 0; i < maxAPICalls; i++) {          
-              idGenAsync.next(new Result<Long>() {
-                @Override
-                public void handle(Long result, Throwable t) {
-                    if(t == null) {
-                      resultsCountDown.countDown();
-                    }                    
-                    else {
-                      Assert.fail("Error yielding result! + " + t);
-                    }
-                }
+	
+		int secondsToRun = 5;
+		int ypsMinExpected = 100_000;    
+		int maxAPICalls = 15_000_000;
 
-              });
-            }            
-          }
-        };
-        
-        // ======================================================================================
-        taskRunner.submit(queryAPI); 
-        resultsCountDown.await(secondsToRun, TimeUnit.SECONDS);
-        long countDown = resultsCountDown.getCount();
-        long resultsYielded = maxAPICalls - countDown;
-        // ======================================================================================
-        taskRunner.shutdownNow();
-        
-        double ypsActual = resultsYielded / secondsToRun;
-        System.out.println("Yielded Results per second: " + ypsActual);
-        
-        assertTrue(
-            "Not enough results yielded! Expected " + ypsMinExpected + " results per second. Was acutally: " + ypsActual,
-            (ypsActual > ypsMinExpected));
-         
-    }
+		CountDownLatch resultsCountDown = new CountDownLatch(maxAPICalls);
+		ExecutorService taskRunner = Executors.newSingleThreadExecutor();
+		Runnable queryAPI = new Runnable() {
+			@Override
+			public void run() {
+				for(int i = 0; i < maxAPICalls; i++) {          
+					idGenAsync.next(new Result<Long>() {
+						@Override
+						public void handle(Long result, Throwable t) {
+							if(t == null) {
+								resultsCountDown.countDown();
+							}
+							else {
+								Assert.fail("Error yielding result! + " + t);
+							}
+						}
+					});
+				}
+			}
+		};
+
+		// ======================================================================================
+		taskRunner.submit(queryAPI); 
+		resultsCountDown.await(secondsToRun, TimeUnit.SECONDS);
+		long countDown = resultsCountDown.getCount();
+		System.out.println("countDown is: " + countDown);
+		long resultsYielded = maxAPICalls - countDown;
+		// ======================================================================================
+		taskRunner.shutdownNow();
+
+		double ypsActual = resultsYielded / secondsToRun;
+		System.out.println("Yielded Results per second: " + ypsActual);
+
+		assertTrue(
+			"Not enough results yielded! Expected " + ypsMinExpected + " results per second. Was acutally: " + ypsActual,
+			(ypsActual > ypsMinExpected));
+	 
+	}
 	
 	
 	private String stringifyId(Long value) {
-      
-      long unixMillis = BarflakeDecoder.decodeTimestamp(value, 1456530296738L);
-      long unixSeconds = unixMillis / 1000;
-      int nanos = (int) (unixMillis % 1000) * 1000000;
-      
-      return " { "
-          + "value: " + value
-        + ", timestamp: " + LocalDateTime.ofEpochSecond(unixSeconds, nanos, ZoneOffset.UTC)
-        + ", sequence: " + BarflakeDecoder.decodeSequence(value)
-        + ", datacenter: " + BarflakeDecoder.decodeDataCenter(value)
-        + ", worker: " + BarflakeDecoder.decodeWorker(value)
-        + " }";
-  }
+	
+		long unixMillis = BarflakeDecoder.decodeTimestamp(value, 1456530296738L);
+		long unixSeconds = unixMillis / 1000;
+		int nanos = (int) (unixMillis % 1000) * 1000000;
+
+		return " { "
+			+ "value: " + value
+			+ ", timestamp: " + LocalDateTime.ofEpochSecond(unixSeconds, nanos, ZoneOffset.UTC)
+			+ ", sequence: " + BarflakeDecoder.decodeSequence(value)
+			+ ", datacenter: " + BarflakeDecoder.decodeDataCenter(value)
+			+ ", worker: " + BarflakeDecoder.decodeWorker(value)
+			+ " }";
+	}
 	
 }
